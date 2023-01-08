d5414a7024080bc8e8d647ae9bcd793f07577400
jonatvaquez <jonatdance1989@gmail.comgit --help>
2023-01-08 03:01:36 -0300

    Tarea de Bucles

---
+++ tarea/Bucle/Bucle_if.java
@@ -0,0 +1,13 @@
+package tarea.Bucle;
+
+public class Bucle_if {
+
+    public static void main(String[] args) {
+    int numeroIf = 0;
+        if (numeroIf <= 10 ) {
+            System.out.print("Es Positivo " +numeroIf);
+        } else {
+            System.out.print("Es Negativo " +numeroIf);
+        }
+    }
+}
